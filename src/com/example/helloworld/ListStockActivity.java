e7634d0c8dd2d253fe18ee2c61358dde58b572df
massimiliano <massimiliano.friz@gmail.com>
2015-08-12 17:36:50 +0200

    aggiunta lista stock da file su telefono

--- src/com/example/helloworld/ListStockActivity.java
+++ src/com/example/helloworld/ListStockActivity.java
@@ -1,11 +1,14 @@
 package com.example.helloworld;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.lang.String;
 
+import android.view.View;
+import android.widget.*;
 import org.apache.http.NameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,18 +23,16 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.TextView;
+
 /**
  * Created by massimiliano on 05/08/15.
  */
 public class ListStockActivity extends ListActivity  {
     //New
     ListView listView;
+
+    static final int READ_BLOCK_SIZE = 100;
 
     ArrayList<HashMap<String, String>> stockList;
     private ProgressDialog progressMessage;
@@ -59,14 +60,19 @@ public class ListStockActivity extends ListActivity  {
         protected String doInBackground(String... args) {
 
             try {
-                ArrayList<String> stocks = new ArrayList<String>();
-                stocks.add("GOOGL");
-                stocks.add("FB");
-                stocks.add("EBAY");
-                for (int i = 0; i < stocks.size(); i++) {
-                    Log.d("FINANZA: ", "test " + stocks.size());
-                    //StockBean stock = StockTickerDAO.getInstance().getStockPrice("GOOGL");
-                    StockBean stock = StockTickerDAO.getInstance().getStockPrice(stocks.get(i));
+                //ArrayList<String> stocks = new ArrayList<String>();
+                ArrayList<String> stocksFile = new ArrayList<String>();
+                //stocks.add("GOOGL");
+                //stocks.add("FB");
+                //stocks.add("EBAY");
+                WriteBtn();
+                stocksFile = ReadBtn();
+                //Check this web site
+//http://android-er.blogspot.it/2011/04/read-text-file-from-internet-using-java.html
+                for (int i = 0; i < stocksFile.size(); i++) {
+                    //Log.d("FINANZA: ", "test " + stocks.size());
+                    StockBean stock = StockTickerDAO.getInstance().getStockPrice(stocksFile.get(i));
+                    //StockBean stock = StockTickerDAO.getInstance().getStockPrice(stocks.get(i));
                     String Stock = stock.getTicker();
 
                     String value = "value: " + stock.getPrice();
@@ -103,5 +109,88 @@ public class ListStockActivity extends ListActivity  {
 
         }
 
+    }
+/*
+    public void getRemoteStockList(){
+        try {
+            File file = new File("http://www.amicofriz.it/fz-webservice/stock.xml");
+            InputStream is = new FileInputStream(file.getPath());
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(is));
+            doc.getDocumentElement().normalize();
+
+            NodeList nodelist = doc.getElementsByTagName("consignment");
+            Node node = nodeList.item(0);
+            Element fstElmnt = (Element) node;
+            String id=fstElmnt.getAttribute("iid");
+            String consignmentId=(String)fstElmnt.Element("consignmentId");
+            String orderCode=(String)fstElmnt.Element("orderCode");
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("XML Pasing Excpetion = " + e);
+        }
+    }
+    */
+
+    public ArrayList ReadBtn() {
+        //reading text from file
+        ArrayList<String> stocks2 = new ArrayList<String>();
+        try {
+            String dir = getFilesDir().toString();
+            Log.d("FINANZA: ", dir);
+            FileInputStream fileIn=openFileInput("stockFile.txt");
+            InputStreamReader InputRead= new InputStreamReader(fileIn);
+
+            //AddFRIZ
+            BufferedReader in = new BufferedReader(InputRead);
+            String line = null;
+            //StringBuilder responseData = new StringBuilder();
+            while((line = in.readLine()) != null) {
+                stocks2.add(line);
+                //responseData.append(line);
+            }
+            /*
+            char[] inputBuffer= new char[READ_BLOCK_SIZE];
+            String s="";
+            int charRead;
+
+            while ((charRead=InputRead.read(inputBuffer))>0) {
+                // char to string conversion
+                String readstring=String.copyValueOf(inputBuffer,0,charRead);
+                s +=readstring;
+                stocks2.add(readstring);
+
+            }
+            */
+            InputRead.close();
+            //Toast.makeText(getBaseContext(), s, Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stocks2;
+    }
+
+    public void WriteBtn() {
+        // add-write text into file
+        try {
+            String dir = getFilesDir().toString();
+            Log.d("FINANZA: ", dir);
+            FileOutputStream fileout=openFileOutput("stockFile.txt", MODE_PRIVATE);
+            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+            outputWriter.write("GOOGL\n");
+            outputWriter.write("FB\n");
+            outputWriter.close();
+
+            //display file saved message
+            //Toast.makeText(getBaseContext(), "File saved successfully!",
+            //        Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/com/example/helloworld/StockXml.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+/**
+ * Created by massimiliano on 11/08/15.
+ */
+public class StockXml {
+    String name = null;
+    String description = null;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
